a98fb5b00c21564ac30938bdf26751c26edebe33
CWY1013HK <65060408+CWY1013HK@users.noreply.github.com>
2020-05-27 23:35:47 +0800

    Update Interface-cd.cpp

--- Source/Interface-cd.cpp
+++ Source/Interface-cd.cpp
@@ -10,6 +10,7 @@
 using namespace std;
 
 int main() {
+    std::system("cd Documents/SenGenLinux");
     std::srand(time(NULL));
     std::system("clear");
     cout << "SENGEN\nCreated by CWY1013HK 2020\nInterface Version : 2.3\n\n";
